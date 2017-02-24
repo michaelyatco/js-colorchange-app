2675d675f7b00e1a9d3f5eca02e16442902e71f8
Michael Yatco <mjyatco@gmail.com>
2017-02-23 21:35:05 -0500

    Added jquery fetch method

--- app/assets/javascripts/pages.js
+++ app/assets/javascripts/pages.js
@@ -17,19 +17,55 @@ function changeColors() {
 }
 
 var response = $.get("https://data.cityofnewyork.us/resource/7drc-shp9.json", function(data) {
-  console.log(data);
-  var htmlString = "<div>";
+  // console.log(data);
+  var htmlString = "";
   for (var i = 0; i < data.length; i++) {
     var medallion = data[i];
-    console.log(medallion);
-    htmlString += "<h1>" + data[i].name + "</h1>";
-    htmlString += "<p>" + data[i].type; + "</p>";
+    htmlString += "<div onclick='changeMedallion(this)'>";
+    htmlString += "<p>" + data[i].name; + "</p>";
+    htmlString += "<p>" + data[i].medallion_type; + "</p>";
+    htmlString += "<p class='model_year'>" + data[i].model_year; + "</p>";
+    htmlString += "<p>" + data[i].vehicle_type; + "</p>";
+    htmlString += "</div>";
   }
-  htmlString += "</div>";
+  // console.log(htmlString);
   document.getElementById('medallion').innerHTML = htmlString;
-  console.log(document.getElementById('medallion').innerHTML);
 });
 
+var colors = ["red", "black", "green", "yellow", "orange"];
+function changeMedallion(medallion) {
+  // if (medallion.style['color'] === 'blue') {
+  // medallion.style['color'] = colors[Math.floor(Math.random() * 5 + 1 )];
+  // } else {
+  //   medallion.style['color'] = "blue"
+  // }
+  medallion.style.color = colors[Math.floor(Math.random() * 5 + 1 )];
+  // medallion.style.display = "none";
+  console.log(medallion.children);
+  for (var i = 0; i < medallion.children.length; i++) {
+    if (i !== 0) {
+      if (medallion.children[i].style.display === "none") {
+        medallion.children[i].style.display = "block";
+      } else {
+        medallion.children[i].style.display = "none";
+      }
+    }
+  }
+}
+
+function modelOnly() {
+  var modelyear = document.getElementsByClassName("model_year");
+  // console.log(modelOnly);
+  for (var i = 0; i < modelyear.length; i++) {
+    var model = modelyear[i];
+    console.log(model.innerHTML.indexOf("2012"));
+    if (model.innerHTML.indexOf("2012") !== -1) {
+      model.style.display = "block";
+    } else {
+      model.style.display = "none";
+    }
+  }
+}
 
 
 // fetch('https://data.cityofnewyork.us/resource/7drc-shp9.json').then(function(response) { 
