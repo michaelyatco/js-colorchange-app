0623e654e128934234c6b042d49d77330b868f21
Michael Yatco <mjyatco@gmail.com>
2017-02-23 20:35:58 -0500

    Added fetch as well as $.get function

--- app/assets/javascripts/pages.js
+++ app/assets/javascripts/pages.js
@@ -1,3 +1,5 @@
+// global $
+
 function changeColors() {
   setTimeout(function() {
     document.getElementById('first').style["color"] = "red";
@@ -13,3 +15,27 @@ function changeColors() {
   }, 1000);  
 
 }
+
+var response = $.get("https://data.cityofnewyork.us/resource/7drc-shp9.json", function(data) {
+  console.log(data);
+  var htmlString = "<div>";
+  for (var i = 0; i < data.length; i++) {
+    var medallion = data[i];
+    console.log(medallion);
+    htmlString += "<h1>" + data[i].name + "</h1>";
+    htmlString += "<p>" + data[i].type; + "</p>";
+  }
+  htmlString += "</div>";
+  document.getElementById('medallion').innerHTML = htmlString;
+  console.log(document.getElementById('medallion').innerHTML);
+});
+
+
+
+// fetch('https://data.cityofnewyork.us/resource/7drc-shp9.json').then(function(response) { 
+//   // Convert to JSON
+//   return response.json();
+// }).then(function(j) {
+//   // Yay, `j` is a JavaScript object
+//   console.log(j); 
+// });
